f6f5bc1fc535cc52516148c0e1206482eb32c705
Jsumulong <sumulong28@gmail.com>
2016-12-29 22:32:15 -0600

    write #pop method for Stack class, pass rspec tests

--- stack.rb
+++ stack.rb
@@ -22,6 +22,22 @@ class Stack
 	end
 
 	def pop
+		if contents.size == 1
+			to_be_returned = contents.get(0)
+			contents.set(0, nil)
+			to_be_returned
+		else
+			new_arr_size = (contents.size - 1)
+			to_be_returned = contents.get(new_arr_size)
+			new_arr = FixedArray.new(new_arr_size)
+			i = 0
+			new_arr_size.times do 
+				new_arr.set(i, contents.get(i))
+				i += 1
+			end
+			@contents = new_arr
+			to_be_returned
+		end
 	end
 
 	def top
